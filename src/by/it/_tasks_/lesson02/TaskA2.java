3cbb81bca44015f107a96ffd10f70d2615c34afa
Sasha Dikan <dzikan.al@gmail.com>
2018-12-01 12:10:15 +0300

    Lesson 2 A

--- src/by/it/_tasks_/lesson02/TaskA2.java
+++ src/by/it/_tasks_/lesson02/TaskA2.java
@@ -9,5 +9,20 @@ package by.it._tasks_.lesson02;
 */
 
 class TaskA2 {
+    private static final String TEXT = "Я начинаю изучать Java!";
+    public static void main (String[] arg) {
+        System.out.println(TEXT);
+        String s = "Я начинаю изучать Java!";
 
-}
+        printTextMoreTimes(s, 5);
+    }
+
+        private static void printTextMoreTimes (String phrase, int count) {
+            for  (int i = 1; i < count; i++) {
+              System.out.println(phrase);
+            }
+        }
+    }
+
+
+
